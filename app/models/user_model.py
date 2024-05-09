ab97d956343cb4c1894e5dfebae5a54cdbf9eee5
FernandoIzidio <luizfernando1010200@gmail.com>
2024-05-09 12:12:25 -0300

    modify user_model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -10,7 +10,16 @@ class User(db.Model):
     phone_number = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     id_balance = db.Column(
-        db.Integer,
-        db.ForeignKey("balance.id"),
-        unique=True,
+        db.Integer, db.ForeignKey("balance.id"), unique=True, nullable=False
     )
+
+    def __init__(self, name, email, phone_number, password, id_balance):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = password
+        self.id_balance = id_balance
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}', '{self.phone_number}', \
+            '{self.password}', '{self.id_balance}')"
