2e20c6533ee8cd1f909e580aef5c9a210f63ce70
FernandoIzidio <luizfernando1010200@gmail.com>
2024-05-09 11:06:38 -0300

    create User Model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,16 @@
-from flask import Blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-app = Blueprint("user_model", __name__)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    phone_number = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    id_balance = db.Column(
+        db.Integer,
+        db.ForeignKey("balance.id"),
+        unique=True,
+    )
