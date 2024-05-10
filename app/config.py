be659e40cc89ebf04a4203d0cf76426f115660c9
FernandoIzidio <luizfernando1010200@gmail.com>
2024-05-10 12:45:17 -0300

    create request functions

--- app/config.py
+++ app/config.py
@@ -2,5 +2,16 @@ from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash
 import os
 
+
 load_dotenv()
 SECRET_KEY = generate_password_hash(os.getenv("SECRET_KEY"))
+BETTING_POINT = "https://api.betfair.com/exchange/betting/rest/v1.0/"
+ACCOUNT_POINT = "https://api.betfair.com/exchange/account/rest/v1.0"
+
+header = {
+    "X-Application": os.getenv("APP_KEY"),
+    "X-Authentication": os.getenv("SESSION_TOKEN"),
+    "content-type": "application/json",
+}
+
+json_req = '{"filter":{ }}'
---
+++ app/utils.py
@@ -0,0 +1,27 @@
+from typing import Literal
+from config import ACCOUNT_POINT, BETTING_POINT, json_req, header
+import requests
+
+
+def request_account_api(resource: str, method: Literal["GET", "POST", "PUT", "DELETE"]):
+    url = ACCOUNT_POINT + resource
+
+    response = getattr(requests, method.lower())(
+        url,
+        data=json_req,
+        headers=header,
+    )
+
+    return response.json()
+
+
+def request_betting_api(resource: str, method: Literal["GET", "POST", "PUT", "DELETE"]):
+    url = BETTING_POINT + resource
+
+    response = getattr(requests, method.lower())(
+        url,
+        data=json_req,
+        headers=header,
+    )
+
+    return response.json()
