fe1da28a52834dd914e472136cd1ef663000f6f3
FernandoIzidio <luizfernando1010200@gmail.com>
2024-05-10 21:51:30 -0300

    create Account.API.get_account_details

--- app/api_interface.py
+++ app/api_interface.py
@@ -4,4 +4,6 @@ from utils import request_account_api, request_betting_api  # noqa
 
 
 class Account_API:
-   
+    @classmethod
+    def get_account_details(cls):
+        return request_account_api("getAccountDetails/", "POST", json_req)
